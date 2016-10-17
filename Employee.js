eaadf728115884b255b9af0fafdd00025dc2a3ac
Trey Howle <howleg@email.uscupstate.edu>
2016-10-17 13:18:04 -0500

    Done

--- Employee.js
+++ Employee.js
@@ -3,12 +3,27 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.name = name
+    this.title = title
+    this.salary = salary
   }
 
-  // TODO ???
+static parseFromFilePath( path ){
+
+let result = JSON.parse(fs.readFileSync(path))
+
+return new Employee(result.name, result.title, result.salary)
+
 }
+
+
+  promote(title,salary){
+    this.title = title
+    this.salary = salary
+  }
+
+}//end of class
 
 // TODO ???
 
